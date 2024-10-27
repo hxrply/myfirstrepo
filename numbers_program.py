d8dd7a4cbdf6d20f77af0ae9ee713db7c113ac3b
hxrply <jackharpley1@gmail.com>
2024-10-27 15:26:02 +0000

    v4
    added prime number identification

--- numbers_program.py
+++ numbers_program.py
@@ -1,14 +1,27 @@
 def is_prime(z):
-    return True
+    if z < 2:
+        return False
+    else:
+        for i in range(2,z):
+            if z % i == 0:
+                #print("No divisible by",i)
+                return False
+        return True
 
+y = int(input("Please enter a start index: "))
+x = int(input("Please enter an end index: "))
 
-y = 1 #int(input("Please enter a start index: "))
-x = 10 #int(input("Please enter an end index: "))
+total_int=0
+total_prime=0
+
 for z in range(y,x+1,1):
-    if is_prime(z) == True:
+    total_int += 1
+    if is_prime(z):
+        total_prime += 1
         print("",z,"*",sep='')
     else:
         print(z)
-
     
-print("Total number of integars printed:", x-y+1)
+
+print("Total number of integars printed:", total_int)
+print("Total number of primes printed:", total_prime)
