88550d3ad0b70c5abbff808614b42d0ed34503b5
hxrply <jackharpley1@gmail.com>
2024-10-27 12:36:11 +0000

    Update numbers_program.py
    is_prime stubbed and main code making calls to is prime in place

--- numbers_program.py
+++ numbers_program.py
@@ -1,6 +1,14 @@
-y = int(input("Please enter a start index: "))
-x = int(input("Please enter an end index: "))
-for z in range(y,x+1,1):
-    print(z)
-print("Total number of integars printed:", x-y+1)
+def is_prime(z):
+    return True
 
+
+y = 1 #int(input("Please enter a start index: "))
+x = 10 #int(input("Please enter an end index: "))
+for z in range(y,x+1,1):
+    if is_prime(z) == True:
+        print("",z,"*",sep='')
+    else:
+        print(z)
+
+    
+print("Total number of integars printed:", x-y+1)
